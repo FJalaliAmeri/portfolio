f04b0d5fc4145b50d56d1920d7c666b2ec6dd373
Farshad <f.jalali.ameri@gmail.com>
2023-03-01 19:03:58 -0500

    Navigation added

---
+++ src/components/Navigation.js
@@ -0,0 +1,49 @@
+import "./Navigation.css";
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const Navigation = () => {
+  const [touch, setTouch] = React.useState(false);
+  const handelTouch = () => {
+    setTouch(!touch);
+  };
+  return (
+    <div className="header">
+      <Link to="/" className="header__logo">
+        <h1>FJA</h1>
+      </Link>
+      <ul className={touch ? "header__nav active" : "header__nav"}>
+        <li className="header__nav-item">
+          <Link to="/about" className="header__nav-link">
+            About
+          </Link>
+        </li>
+        <li className="header__nav-item">
+          <Link to="/resume" className="header__nav-link">
+            Resume
+          </Link>
+        </li>
+        <li className="header__nav-item">
+          <Link to="/projects" className="header__nav-link">
+            Projects
+          </Link>
+        </li>
+        <li className="header__nav-item">
+          <Link to="/contact" className="header__nav-link">
+            Contact
+          </Link>
+        </li>
+      </ul>
+      <div className="header__nav-hamburger" onClick={handelTouch}>
+        {touch ? (
+          <FaTimes className="header__hamburger-icon" onClick={handelTouch} />
+        ) : (
+          <FaBars className="header__hamburger-icon" onClick={handelTouch} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
